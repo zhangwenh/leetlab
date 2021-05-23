2c3495f8cae503c39949be59dc1d8c52ed94c864
zhangwh <zhangwenhster@gmail.com>
2021-05-23 10:45:59 +0800

    Update Solution.java

--- src/main/java/io/rdk/leetlab/core/Backtracking/p46_permutations/Solution.java
+++ src/main/java/io/rdk/leetlab/core/Backtracking/p46_permutations/Solution.java
@@ -1,35 +1,43 @@
 package io.rdk.leetlab.core.Backtracking.p46_permutations;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
+/**
+ * dfs+回溯
+ * 详见官方视频 https://leetcode-cn.com/problems/permutations/solution/quan-pai-lie-by-leetcode-solution-2/
+ */
 public class Solution {
     public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        HashMap<Integer, Boolean> visited = new HashMap<>();
-        for (int num : nums) {
-            visited.put(num, false);
+        List<List<Integer>> res = new ArrayList<>();
+        if(nums == null || nums.length==0){
+            return res;
         }
-        backtracking(nums, result, visited, new ArrayList<>());
-        return result;
+        // 状态变量
+        int len = nums.length;
+        Deque<Integer> path = new ArrayDeque<>();
+        boolean[] used = new boolean[len];
+
+        dfs(nums,len,0,path,used,res);
+        return res;
     }
 
-    private void backtracking(int[] nums, List<List<Integer>> result, HashMap<Integer, Boolean> visited, ArrayList<Integer> list) {
-        if (list.size() == nums.length) {
-            result.add(new ArrayList<>(list));
+    private void dfs(int[] nums, int len, int depth, Deque<Integer> path, boolean[] used, List<List<Integer>> res) {
+        // 退出条件
+        if(depth == len){
+            res.add(new ArrayList<>(path));
             return;
         }
-        for (int i = 0; i < nums.length; i++) {
-            int num = nums[i];
-            if (!visited.get(num)) {
-                list.add(num);
-                visited.put(num, true);
-                backtracking(nums, result, visited, list);
-                list.remove(list.size() - 1);
-                visited.put(num, false);
+        for (int i = 0; i < len; i++) {
+            if(used[i]){ // 当前数字已经被使用，则跳过
+                continue;
             }
+            path.addLast(nums[i]);
+            used[i] = true;
+            dfs(nums,len,depth+1,path,used,res);
+            path.removeLast(); // 回溯逆操作
+            used[i] = false;
         }
-
     }
+
+
 }
