0ef5305cb9e341f008934b75ee1a8c9171238e96
zhangwh <zhangwenhster@gmail.com>
2021-05-25 22:23:07 +0800

    Update Solution.java

--- src/main/java/io/rdk/leetlab/core/Memozation/p509_fibonacci_number/Solution.java
+++ src/main/java/io/rdk/leetlab/core/Memozation/p509_fibonacci_number/Solution.java
@@ -4,12 +4,21 @@ package io.rdk.leetlab.core.Memozation.p509_fibonacci_number;
  *
  */
 public class Solution {
+    int[] memArr = new int[30]; // 根据题意最大不会超过30
+
     public int fib(int n) {
-        if(n<2){
-            return n;
-        }else {
-            return fib(n-1)+fib(n-2);
+        int ret = 0;
+        if(memArr[n] != 0){
+            ret = memArr[n];
+        }else{
+            if(n<2){
+                ret = n;
+            }else {
+                ret = fib(n-1)+fib(n-2);
+            }
+            memArr[n] = ret;
         }
+        return ret;
     }
 
     public static void main(String[] args) {
