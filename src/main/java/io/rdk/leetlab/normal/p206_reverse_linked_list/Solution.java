4e1d4bb72d258d30ee82e12b3479c5f3f5cf8ae2
zhangwh <zhangwenhster@gmail.com>
2021-08-13 09:54:22 +0800

    Create Solution.java

---
+++ src/main/java/io/rdk/leetlab/normal/p206_reverse_linked_list/Solution.java
@@ -0,0 +1,36 @@
+package io.rdk.leetlab.normal.p206_reverse_linked_list;
+
+import io.rdk.leetlab.pub.ListNode;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        // 迭代法
+        // 1、设定新节点dm，作为头，最后将返回dm.next
+        // dm->1->2->3->null
+        // 2、循环遍历，直到head.next指向null
+        // 3、循环中，设定新节点tmp，为head.next
+        // dm->1(head)->2(tmp)->3->null
+        // 4、各种翻转:
+        // dm->2(tmp)->1(head)->3->null
+        // dm->3(tmp)->2->1(head)->null
+        ListNode dm = new ListNode();
+        dm.next = head;
+        while(head!=null && head.next != null){
+            ListNode tmp = head.next;
+            head.next = tmp.next;
+            tmp.next = dm.next;
+            dm.next = tmp;
+        }
+        return dm.next;
+    }
+}
