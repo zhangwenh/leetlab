fd2e407e0458c80dae960574e5a8cadee7039e6a
zhangwh <zhangwenhster@gmail.com>
2021-08-15 21:47:37 +0800

    Create Solution.java

---
+++ src/main/java/io/rdk/leetlab/normal/p287_find_the_duplicate_number/Solution.java
@@ -0,0 +1,9 @@
+package io.rdk.leetlab.normal.p287_find_the_duplicate_number;
+
+public class Solution {
+    public int findDuplicate(int[] nums) {
+        int res = -1;
+        // todo
+        return res;
+    }
+}
