c3f03f351d2df2171d7d3ce8d06758ec4fc25423
zhangwh <zhangwenhster@gmail.com>
2021-08-16 15:07:09 +0800

    Update Solution.java

--- src/main/java/io/rdk/leetlab/normal/p720_longest_word_in_dictionary/Solution.java
+++ src/main/java/io/rdk/leetlab/normal/p720_longest_word_in_dictionary/Solution.java
@@ -2,6 +2,14 @@ package io.rdk.leetlab.normal.p720_longest_word_in_dictionary;
 
 public class Solution {
     public String longestWord(String[] words) {
+        // 暴力法
+        String res = "";
+        // todo
+        return res;
+    }
+
+    public String longestWord1(String[] words) {
+        // trie法
         String res = "";
         // todo
         return res;
