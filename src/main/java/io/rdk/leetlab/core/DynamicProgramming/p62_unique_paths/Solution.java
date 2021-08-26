7e4c145e015ec1e86df9659ee99f1d378dc319e3
zhangwh <zhangwenhster@gmail.com>
2021-08-26 09:34:02 +0800

    Update Solution.java

--- src/main/java/io/rdk/leetlab/core/DynamicProgramming/p62_unique_paths/Solution.java
+++ src/main/java/io/rdk/leetlab/core/DynamicProgramming/p62_unique_paths/Solution.java
@@ -6,13 +6,14 @@ public class Solution {
         // 初始状态：1
         // 终止状态：F(i,j)
         int[][] dp = new int[m][n];
+        // 两个边的处理
         for (int i = 0; i < m; i++) {
             dp[i][0] = 1;
         }
         for (int i = 0; i < n; i++) {
             dp[0][i] = 1;
         }
-        for (int i = 1; i < m; i++) {
+        for (int i = 1; i < m; i++) { // 注意这2个循环都是从1开始
             for (int j = 1; j < n; j++) {
                 dp[i][j] = dp[i-1][j] + dp[i][j-1];
             }
